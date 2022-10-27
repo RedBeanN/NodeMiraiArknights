4b0b9291cd280489a3984b2588e69fbf4f185506
RedBeanN <me@hongshn.xyz>
2022-10-27 23:25:20 +0800

    not printing empty message

--- src/components/messageLogger.ts
+++ src/components/messageLogger.ts
@@ -4,7 +4,7 @@ import datetime from '../../utils/datetime';
 const messageLogger = new Component('messageLogger', [{
   rules: [/.*/],
   handler ({ senderId, senderName, msg, isMaster, config }) {
-    if (!config.logMessage) return;
+    if (!config.logMessage || !msg) return;
     console.log(`[Arknights][${datetime()}]${isMaster ? '*' : ''} ${senderName}(${senderId}): ${msg}`);
   },
 }]);
